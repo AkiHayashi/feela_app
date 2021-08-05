99a5b0b443e721955e1e2514e7ba9ad7f37d3a97
aki hayashi <hayashi1019toy@gmail.com>
2021-08-05 21:28:21 +0900

    function of edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,19 @@ class PostsController < ApplicationController
       end
     end
   end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "successfuly edited!"
+    else
+      render :edit
+    end
+  end
+  def show
+  end
 
   private
   def post_params
