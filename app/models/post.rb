34b1aeb4b0c22f4155f9fbeff6c78458e75333c6
aki hayashi <hayashi1019toy@gmail.com>
2021-08-06 17:12:04 +0900

    modify gemfile

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,3 @@
 class Post < ApplicationRecord
-  validates :content, presence: true
-  validates :content, length:  { maximum: 140}
-  validates :content, length: {minimum: 1}
+  validates :content, presence: true, length: {maximum: 140}
 end
